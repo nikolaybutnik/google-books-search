05d30c0d3e6bc0e7eb8cb38148c82b3f87c81ad0
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-01-23 18:32:40 -0500

    Add styling to search results.

--- client/src/components/Volume.js
+++ client/src/components/Volume.js
@@ -21,17 +21,31 @@ function Volume({ title, authors, description, image, link }) {
   }
 
   return (
-    <div>
+    <div
+      style={{
+        border: 'solid black thin',
+        margin: '10px',
+        marginTop: '20px',
+        padding: '10px',
+        backgroundColor: '#F0F0F0',
+      }}
+    >
       <h3>{title}</h3>
-      <img src={image} alt={title} />
-      <button>
-        <a href={link} target="_blank" rel="noreferrer">
-          View Book
-        </a>
-      </button>
-      <button onClick={handleBookSave}>Save Book</button>
-      <p>Authors: {authors}</p>
-      <p>Description: {description}</p>
+      <img src={image} alt={title} style={{ border: 'Solid 5px #C83E3E' }} />
+      <div style={{ margin: '10px', marginLeft: '0px' }}>
+        <button>
+          <a href={link} target="_blank" rel="noreferrer">
+            View Book
+          </a>
+        </button>
+        <button onClick={handleBookSave}>Save Book</button>
+      </div>
+      <p>
+        <h3>Authors:</h3> {authors}
+      </p>
+      <p>
+        <h3>Description:</h3> {description}
+      </p>
     </div>
   )
 }
